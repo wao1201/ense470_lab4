89a34c2f7e918ba34a71be470cb634f4157e6bf2
Wao <wao1201@gmail.com>
2014-02-27 20:47:34 -0600

    Initial commits
    lab4 code and writeup

---
+++ 470lab4.cpp
@@ -0,0 +1,38 @@
+// ense470 lab4: osg test
+// Name: Wei Wang 200 272 278
+// description: a simple program that allow user to load osg models as they wish
+
+#include <osgViewer/Viewer>
+#include <osg/Group>
+#include <osgDB/ReadFile> 
+#include <iostream>
+
+int main(){
+	// create group node
+	osg::ref_ptr<osg::Group> rootNode = new osg::Group;
+
+	// read model files
+	osg::ref_ptr<osg::Node> model1 = osgDB::readNodeFile("cow.osg");
+	osg::ref_ptr<osg::Node> model2 = osgDB::readNodeFile("avatar.osg");
+	osg::ref_ptr<osg::Node> model3 = osgDB::readNodeFile("cessnafire.osg");
+	
+	// ask user to choose one model to load
+	int modelNum;
+	cout<<"Please choose a model to load by entering number (1, 2,or 3): ";
+	cin>>modelNum;
+	
+	// based on user choice, load model
+	if(modelNum==1)
+		rootNode->addChild(model1.get());
+	else if(modelNum==2)
+		rootNode->addChild(model2.get());
+	else if(modelNum==3)
+		rootNode->addChild(model3.get());
+
+	// pass the root to viewer
+	osgViewer::Viewer viewer;
+	viewer.setSceneData( rootNode.get());
+	viewer.setUpViewInWindow(50,50,800,600); // instead of full screen, set a window
+	viewer.run();
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 ense470_lab4
 ============
+
+This is a warmup practice in order to get familiar with osg programming.
+
+The program simply provides three options to user to view osg models:
+1. cow.osg
+2. avatar.osg
+3. cessnafire.osg
+
+After user chose one option, the corresponding model would be loaded and viewed in a window on screen.
